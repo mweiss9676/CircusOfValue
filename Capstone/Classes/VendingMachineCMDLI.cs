3000f773ff8ad6593b0fe1cc3650df3fb9da3e64
Anna Penta <apenta@kent.edu>
2018-02-08 17:39:14 -0500

    Anna's version 2.8

--- Capstone/Classes/VendingMachineCMDLI.cs
+++ Capstone/Classes/VendingMachineCMDLI.cs
@@ -3,41 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capstone;
 
 
 namespace Capstone.Classes
 {
     public class VendingMachineCMDLI
     {
+        static VendingMachineLogic machine = new VendingMachineLogic();
+
         static void Main(string[] args)
         {
-            while(true)
+            
+
+            while (true)
             {
                 try
                 {
-                    Console.WriteLine("(1) Display Items");
-                    int displayItems = int.Parse(Console.ReadLine());
+                    
 
-                    VendingMachineLogic machine = new VendingMachineLogic();
+                   
 
-                    foreach (var kvp in machine.Inventory)
-                    {
-                        Console.WriteLine($"{kvp.Key}| {kvp.Value[0].NameOfItem} {kvp.Value[2].PriceOfItem}");
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("(2) Purchase");
-                    int purchaseItems = int.Parse(Console.ReadLine());
+                    
 
-                    Console.WriteLine("(1) Feed Money");
-                    int feedMoney = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine("(2) Select Product");
-                    int selectProduct = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine("(3) Finish Transaction");
-                    int finishTransaction = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine($"Current Money Provided: ***");
+                   
                     break;
 
                 }
@@ -47,6 +36,46 @@ namespace Capstone.Classes
                     Console.WriteLine("nah bruj");
                 }
             }
+        }
+        private static void PrintDisplayMenu(VendingMachineLogic machine)
+        {
+            Console.WriteLine("(1) Display Items");
+            Console.WriteLine();
+            Console.WriteLine("(2) Purchase Items");
+            int mainMenuResult = int.Parse(Console.ReadLine());
+
+            if(mainMenuResult == 1)
+            {
+                PrintDisplayItems(machine);
+            }
+            else if(mainMenuResult == 2)
+            {
+                PrintPurchaseMenu(mainMenuResult, machine);
+            }
+
+        }
+        private static void PrintDisplayItems(VendingMachineLogic machine)
+        {
+            foreach (var kvp in machine.Inventory)
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value[0].NameOfItem} | {kvp.Value[2].PriceOfItem}");
+            }
+        }
+    private static void PrintPurchaseMenu(int mainMenuResult, VendingMachineLogic machine)
+        {
+           
+           
+                 Console.WriteLine("(1) Feed Money");
+                    int feedMoney = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("(2) Select Product");
+                    int selectProduct = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("(3) Finish Transaction");
+                    int finishTransaction = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine($"Current Money Provided: {machine.CurrentMoneyProvided} ");
+            
         }
     }
 }
--- Capstone/Classes/VendingMachineLogic.cs
+++ Capstone/Classes/VendingMachineLogic.cs
@@ -7,7 +7,7 @@ using Capstone.Classes;
 
 namespace Capstone
 {
-    public class VendingMachineLogic
+   public class VendingMachineLogic
     {
         public Dictionary<string, List<VendingMachineItem>> Inventory { get; }
 
@@ -15,10 +15,18 @@ namespace Capstone
 
         public string[] Slots { get; }
 
+        public decimal CurrentMoneyProvided { get; private set; }
+
         public VendingMachineLogic()
         {
             VendingMachineFileReader filereader = new VendingMachineFileReader();
             Inventory = filereader.GetInventory();
         }
+
+        public void CurrentMoney(decimal feedMoney)
+        {
+            CurrentMoneyProvided += feedMoney;
+        }
+
     }
 }
